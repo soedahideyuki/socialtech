c09dbb4b83b57608130359bf3e0de3771a880f98
soedahideyuki <kiyudehi811@gmail.com>
2023-11-29 09:21:23 +0900

    メインビジュアルをカルーセルにしよう#1

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('.button-more').on('mouseover' , function(){
+    $(this).animate({
+      opacity: 0.5,
+      marginLeft:20,
+    },100);
+    });
+  
+  $('.button-more').on('mouseout' , function(){
+    $(this).animate({
+      marginLeft: 0 ,
+      opacity: 1 , 
+    },100);
+  });
+});
