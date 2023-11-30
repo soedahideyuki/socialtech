7ae5ea0e89da6de0347e8473fd0109283789e0ae
soedahideyuki <kiyudehi811@gmail.com>
2023-11-30 16:56:29 +0900

    入力チェックしよう

--- script.js
+++ script.js
@@ -20,5 +20,37 @@ $(function(){
     autoplayspeed: 5000,
     arrows: false,
   });
+
+  // AjaxでSTATIC　FORMSにデータを送信
+  $('#submit').on('click' , function(event){
+    // formタグによる送信を拒否
+    event.preventDefault();
+    // 入力チェックをした結果、エラーがあるかないか判断
+    let result = inputCheck();
+  });
+
+  // フォーカスが外れた時(blur)にフォームの入力チェックをする
+  $('#name').blur(function(){
+    inputCheck();
+  });
+  $('#furigana').blur(function(){
+    inputCheck();
+  });
+  $('#email').blur(function(){
+    inputCheck();
+  });
+  $('#tel').blur(function(){
+    inputCheck();
+  });
+  $('#message').blur(function(){
+    inputCheck();
+  });
+  $('#agree').click(function(){  //on('click',function)　の書き方でないのか
+    inputCheck();
+  });
+
+  inputCheck=()=>{//質問　これはjQuery??
+    console.log('inputCheck関数の呼び出し');
+  }  
 });
 
